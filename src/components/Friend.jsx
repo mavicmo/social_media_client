1b38f8da249c9c9ada96adc51f9bd2ba8bf1b782
mtaraq5@gmail.com <mtaraq@Mos-MacBook-Pro.home>
2023-02-22 08:41:45 -0500

    postwidget complete

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -64,7 +64,16 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
           </Typography>
         </Box>
       </FlexBetween>
-        <IconButon
+      <IconButon
+        onClick={() => patchFriend()}
+        sx={{ backgroundColor: primaryLight, p: "0.6rem" }}
+      >
+        {isFriend ? (
+          <PersonRemovedOutlined sx={{ color: primaryDark }} />
+        ) : (
+          <PersonAddOutlined sx={{ color: primaryDark }} />
+        )}
+      </IconButon>
     </FlexBetween>
   );
 };
